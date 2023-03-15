a49f32c9b761dedab5d8fa08a7941ac82f268600
shah2032 <tejal941982@gmail.com>
2023-03-15 18:17:57 +0000

    My written homework week 1

---
+++ src/week1writingprogramme/Programme_2_PrintFace.java
@@ -0,0 +1,18 @@
+package week1writingprogramme;
+
+/**
+ * 2.Write a Java programme to print a face.
+ */
+public class Programme_2_PrintFace {
+    public static void main(String[] args) {
+        System.out.println(" +\"\"\"\"\"\"+");
+        System.out.println("[| O   O | ]");
+        System.out.println(" |   ^   |  ");
+        System.out.println(" |  '_'  |  ");
+        System.out.println(" +_______+   ");
+
+    }
+
+
+}
+
